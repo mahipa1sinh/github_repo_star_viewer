2f95b5e325e4a53b4dbd3b2f10ce9e879df92c08
Mahipalsinhnakum <nakummahipalsinh007@gmail.com>
2022-07-11 11:29:03 +0530

    splash screen, app Icons and app lable name changed

--- lib/core/presentation/app_widget.dart
+++ lib/core/presentation/app_widget.dart
@@ -35,6 +35,7 @@ class AppWidget extends ConsumerWidget {
         authenticated: (_) {
           appRouter.pushAndPopUntil(
             const StarredReposRoute(),
+            // const SplashRoute(),
             predicate: (route) =>
                 false, // msking sure user can't go back to splash page
           );
--- lib/splash/presentation/splash_page.dart
+++ lib/splash/presentation/splash_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
 class SplashPage extends StatelessWidget {
@@ -12,14 +13,41 @@ class SplashPage extends StatelessWidget {
           horizontal: 48.0,
         ),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const <Widget>[
-            Icon(
-              MdiIcons.github,
-              size: 150,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Lottie.asset(
+              'assets/animations/loading.json',
+              repeat: true,
+              reverse: false,
+              animate: true,
             ),
-            SizedBox(height: 16),
-            LinearProgressIndicator(),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                RichText(
+                  text: const TextSpan(
+                      style: TextStyle(
+                        color: Color.fromARGB(255, 0, 128, 91),
+                        fontSize: 20,
+                      ),
+                      children: [
+                        TextSpan(text: 'Buckle Up'),
+                      ]),
+                ),
+                RichText(
+                  text: const TextSpan(
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 30,
+                      ),
+                      children: [
+                        TextSpan(text: 'Launching your StarShip!!!'),
+                      ]),
+                ),
+                const Icon(MdiIcons.rocketLaunch),
+              ],
+            ),
           ],
         ),
       ),
